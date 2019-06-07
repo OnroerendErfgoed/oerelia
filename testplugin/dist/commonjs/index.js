3a97e2bc02a62d41209b31d23ced3d3c80f75a69
Axel Verstappen <axel.verstappen@gmail.com>
2019-06-07 11:31:30 +0200

    Tested with dev-app

--- testplugin/dist/commonjs/index.js
+++ testplugin/dist/commonjs/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var aurelia_framework_1 = require("aurelia-framework");
 function configure(config) {
-    config.feature(aurelia_framework_1.PLATFORM.moduleName('oerelia/testplugin/elements/index'));
+    config.feature(aurelia_framework_1.PLATFORM.moduleName('./elements/index'));
 }
 exports.configure = configure;
