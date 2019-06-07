cd5448dee6bfecbcd348a5a8b8f54213bf8ab02e
Axel Verstappen <axel.verstappen@gmail.com>
2019-06-07 15:07:16 +0200

    New plugin

--- testplugin/dist/native-modules/index.js
+++
@@ -1,7 +0,0 @@
-import { PLATFORM } from 'aurelia-framework';
-export * from './hello-world';
-export function configure(config) {
-    config.globalResources([
-        PLATFORM.moduleName('./hello-world')
-    ]);
-}
