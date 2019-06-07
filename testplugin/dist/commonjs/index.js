be8025d64c8d77650dc51acadf4b3918d7f34186
Axel Verstappen <axel.verstappen@gmail.com>
2019-06-07 10:36:06 +0200

    Moved component to root folder

--- testplugin/dist/commonjs/index.js
+++ testplugin/dist/commonjs/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var aurelia_pal_1 = require("aurelia-pal");
 function configure(config) {
     config.globalResources([
-        aurelia_pal_1.PLATFORM.moduleName('./elements/hello-world')
+        aurelia_pal_1.PLATFORM.moduleName('./hello-world')
     ]);
 }
 exports.configure = configure;
