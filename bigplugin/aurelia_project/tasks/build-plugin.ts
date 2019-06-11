8ab38b11e5a441a69bb8822c072573c0d640eb3b
Axel Verstappen <axel.verstappen@gmail.com>
2019-06-11 14:35:47 +0200

    Output only native-modules

--- bigplugin/aurelia_project/tasks/build-plugin.ts
+++ bigplugin/aurelia_project/tasks/build-plugin.ts
@@ -12,17 +12,11 @@ function clean() {
 export default gulp.series(
   clean,
   gulp.parallel(
-    // package.json "module" field pointing to dist/native-modules/index.js
-    pluginMarkup('dist/native-modules'),
-    pluginCSS('dist/native-modules'),
-    pluginJson('dist/native-modules'),
-    buildPluginJavaScript('dist/native-modules', 'es2015'),
-
-    // package.json "main" field pointing to dist/native-modules/index.js
-    pluginMarkup('dist/commonjs'),
-    pluginCSS('dist/commonjs'),
-    pluginJson('dist/commonjs'),
-    buildPluginJavaScript('dist/commonjs', 'commonjs'),
+    // package.json "module" field pointing to dist/index.js
+    pluginMarkup('dist'),
+    pluginCSS('dist'),
+    pluginJson('dist'),
+    buildPluginJavaScript('dist', 'es2015'),
   ),
-  () => console.log('Finish building Aurelia plugin to dist/commonjs and dist/native-modules')
+  () => console.log('Finish building Aurelia plugin to dist')
 );
