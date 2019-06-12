3d23305b1060a41a10d9d150a7c7dbeea0fb783d
Axel Verstappen <axel.verstappen@gmail.com>
2019-06-12 10:16:54 +0200

    Fixed index.js issue

--- dist/native-modules/index.js
+++ dist/native-modules/index.js
@@ -1,6 +1,6 @@
 import { PLATFORM } from 'aurelia-framework';
 export function configure(config) {
     config
-        .feature(PLATFORM.moduleName('oerelia/elements'))
-        .feature(PLATFORM.moduleName('oerelia/tabs'));
+        .feature(PLATFORM.moduleName('oerelia/tabs'))
+        .feature(PLATFORM.moduleName('oerelia/zoneerder'));
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   config
-    .feature(PLATFORM.moduleName('oerelia/elements'))
-    .feature(PLATFORM.moduleName('oerelia/tabs'));
+    .feature(PLATFORM.moduleName('oerelia/tabs'))
+    .feature(PLATFORM.moduleName('oerelia/zoneerder'));
 }
