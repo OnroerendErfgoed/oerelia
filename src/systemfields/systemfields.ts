dc8f64763c7b924b0bd4156d0f56ceb4185d0495
Yannick Kuypers <Kuypers.y@gmail.com>
2019-10-01 15:41:12 +0200

    Added a test systemfields component to see if it reaches to other projects

---
+++ src/systemfields/systemfields.ts
@@ -0,0 +1,24 @@
+// import { bindable } from 'aurelia-framework';
+// import * as moment from 'moment';
+// import { ISystemFields, IStatus } from '../models/apiModel';
+
+// export class Systemfields {
+//   @bindable public systemfields: ISystemFields;
+//   @bindable public status: IStatus;
+//   public createdBy: string;
+//   public createdAt: string;
+//   public updatedBy: string;
+//   public updatedAt: string;
+
+//   public formatDate(date) {
+//     return moment(date).format('DD/MM/YYYY [om] HH:mm');
+//   }
+
+//   public attached() {
+//     this.createdAt = this.formatDate(this.systemfields.created_at);
+//     this.createdBy = this.systemfields.created_by.description;
+
+//     this.updatedAt = this.formatDate(this.systemfields.updated_at);
+//     this.updatedBy = this.systemfields.updated_by.description;
+//   }
+// }
