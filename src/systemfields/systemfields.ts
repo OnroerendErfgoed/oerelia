13743888dd9bf62066e3e518f82c0a2d663173ce
Yannick Kuypers <Kuypers.y@gmail.com>
2019-10-02 09:58:05 +0200

    commented systemfields.ts to see if component without bindings is working

--- src/systemfields/systemfields.ts
+++ src/systemfields/systemfields.ts
@@ -1,25 +1,25 @@
-import { bindable } from 'aurelia-framework';
+// import { bindable } from 'aurelia-framework';
 // import * as moment from 'moment';
-import { ISystemFields } from './models/ISystemFields';
+// import { ISystemFields } from './models/ISystemFields';
 // import { IStatus } from './models/IStatus';
 
 export class Systemfields {
-  @bindable public systemfields: ISystemFields;
+  // @bindable public systemfields: ISystemFields;
   // @bindable public status: IStatus;
-  public createdBy: string;
+  // public createdBy: string;
   // public createdAt: string;
-  public updatedBy: string;
+  // public updatedBy: string;
   // public updatedAt: string;
 
   // public formatDate(date) {
   //   return moment(date).format('DD/MM/YYYY [om] HH:mm');
   // }
 
-  public attached() {
-    // this.createdAt = this.formatDate(this.systemfields.created_at);
-    this.createdBy = this.systemfields.created_by.description;
+  // public attached() {
+  //   this.createdAt = this.formatDate(this.systemfields.created_at);
+  //   this.createdBy = this.systemfields.created_by.description;
 
-    // this.updatedAt = this.formatDate(this.systemfields.updated_at);
-    this.updatedBy = this.systemfields.updated_by.description;
-  }
+  //   this.updatedAt = this.formatDate(this.systemfields.updated_at);
+  //   this.updatedBy = this.systemfields.updated_by.description;
+  // }
 }
