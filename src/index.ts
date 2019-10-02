2dd35063ab6835ce8c418a531ee536000348fe9d
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2019-10-02 14:29:30 +0200

    re-add trailing semicolon

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export function configure(config: FrameworkConfiguration) {
     .feature(PLATFORM.moduleName('oerelia/tabs/index'))
     .feature(PLATFORM.moduleName('oerelia/zoneerder/index'))
     .feature(PLATFORM.moduleName('oerelia/autocomplete/index'))
-    .feature(PLATFORM.moduleName('oerelia/systemfields/index'))
+    .feature(PLATFORM.moduleName('oerelia/systemfields/index'));
 }
