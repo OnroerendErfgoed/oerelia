a514b577442c628149e8d5b96265c21d7eb3c9f7
Axel Verstappen <axel.verstappen@gmail.com>
2019-10-02 15:30:56 +0200

    Removed gis-utils as html components

--- dist/native-modules/gis-utils/index.js
+++ dist/native-modules/gis-utils/index.js
@@ -1,9 +1,5 @@
-import { PLATFORM } from 'aurelia-framework';
 export * from './projection-util';
 export * from './map-util';
 export function configure(config) {
-    config.globalResources([
-        PLATFORM.moduleName('./projection-util'),
-        PLATFORM.moduleName('./map-util')
-    ]);
+    config.globalResources([]);
 }
--- src/gis-utils/index.ts
+++ src/gis-utils/index.ts
@@ -1,11 +1,8 @@
-import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+import { FrameworkConfiguration } from 'aurelia-framework';
 
 export * from './projection-util';
 export * from './map-util';
 
 export function configure(config: FrameworkConfiguration) {
-  config.globalResources([
-    PLATFORM.moduleName('./projection-util'),
-    PLATFORM.moduleName('./map-util')
-  ]);
+  config.globalResources([]);
 }
