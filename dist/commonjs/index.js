485ce853b67de16b1a9b068d85704e987e98c4d1
Axel Verstappen <axel.verstappen@gmail.com>
2019-10-02 13:39:32 +0200

    Added to index.ts

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -5,6 +5,7 @@ function configure(config) {
     config
         .feature(aurelia_framework_1.PLATFORM.moduleName('oerelia/tabs/index'))
         .feature(aurelia_framework_1.PLATFORM.moduleName('oerelia/zoneerder/index'))
-        .feature(aurelia_framework_1.PLATFORM.moduleName('oerelia/autocomplete/index'));
+        .feature(aurelia_framework_1.PLATFORM.moduleName('oerelia/autocomplete/index'))
+        .feature(aurelia_framework_1.PLATFORM.moduleName('oerelia/gis-utils/index'));
 }
 exports.configure = configure;
