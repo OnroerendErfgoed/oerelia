d548f50707550dbd79172b22010572219f5e81c4
Yannick Kuypers <Kuypers.y@gmail.com>
2019-10-02 10:44:33 +0200

    Reverted to correct implementation and added momentjs

--- dist/commonjs/systemfields/systemfields.d.ts
+++ dist/commonjs/systemfields/systemfields.d.ts
@@ -1,2 +1,12 @@
+import { ISystemFields } from './models/ISystemFields';
+import { IStatus } from './models/IStatus';
 export declare class Systemfields {
+    systemfields: ISystemFields;
+    status: IStatus;
+    createdBy: string;
+    createdAt: string;
+    updatedBy: string;
+    updatedAt: string;
+    formatDate(date: any): string;
+    attached(): void;
 }
