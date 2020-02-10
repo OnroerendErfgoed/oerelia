8f61cac2c8a96b17ee53611d791d74fa0525e311
Axel Verstappen <axel.verstappen@gmail.com>
2020-02-10 16:30:39 +0100

    Updated message utility

--- dist/commonjs/utilities/message/message.js
+++ dist/commonjs/utilities/message/message.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var toastr = require("toastr");
 var messageTypes_1 = require("./enums/messageTypes");
@@ -14,14 +25,16 @@ var Message = (function () {
         return new Message(messageTypes_1.messageType.info, config);
     };
     Message.success = function (config) {
-        config.emitterOptions = { timeOut: 10000, extendedTimeOut: 5000 };
+        config.emitterOptions = config.emitterOptions || {};
+        config.emitterOptions = __assign({ timeOut: 10000, extendedTimeOut: 5000 }, config.emitterOptions);
         return new Message(messageTypes_1.messageType.success, config);
     };
     Message.warning = function (config) {
         return new Message(messageTypes_1.messageType.warning, config);
     };
     Message.error = function (config) {
-        config.emitterOptions = { timeOut: 0, extendedTimeOut: 0, closeButton: true };
+        config.emitterOptions = config.emitterOptions || {};
+        config.emitterOptions = __assign({ timeOut: 0, extendedTimeOut: 0, closeButton: true }, config.emitterOptions);
         return new Message(messageTypes_1.messageType.error, config);
     };
     Message.prototype.show = function (type, config) {
--- dist/native-modules/utilities/message/message.js
+++ dist/native-modules/utilities/message/message.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import * as toastr from 'toastr';
 import { messageType } from './enums/messageTypes';
 var Message = (function () {
@@ -12,14 +23,16 @@ var Message = (function () {
         return new Message(messageType.info, config);
     };
     Message.success = function (config) {
-        config.emitterOptions = { timeOut: 10000, extendedTimeOut: 5000 };
+        config.emitterOptions = config.emitterOptions || {};
+        config.emitterOptions = __assign({ timeOut: 10000, extendedTimeOut: 5000 }, config.emitterOptions);
         return new Message(messageType.success, config);
     };
     Message.warning = function (config) {
         return new Message(messageType.warning, config);
     };
     Message.error = function (config) {
-        config.emitterOptions = { timeOut: 0, extendedTimeOut: 0, closeButton: true };
+        config.emitterOptions = config.emitterOptions || {};
+        config.emitterOptions = __assign({ timeOut: 0, extendedTimeOut: 0, closeButton: true }, config.emitterOptions);
         return new Message(messageType.error, config);
     };
     Message.prototype.show = function (type, config) {
