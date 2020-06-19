bf6766eaf1b260eb9390824351621141e2c549dc
Yannick Kuypers <Kuypers.y@gmail.com>
2020-06-19 10:09:15 +0200

    56 fix added ag-grid and rest of actor-widget

--- dist/commonjs/actor-widget/actor-widget.js
+++ dist/commonjs/actor-widget/actor-widget.js
@@ -1,22 +1,259 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var aurelia_templating_1 = require("aurelia-templating");
+var crab_api_service_1 = require("services/crab.api-service");
+var aurelia_framework_1 = require("aurelia-framework");
 var ActorWidget = (function () {
-    function ActorWidget() {
+    function ActorWidget(crabService) {
+        var _this = this;
+        this.crabService = crabService;
+        this.showSpinner = true;
+        this.showTable = true;
         this.showActor = false;
         this.showFilters = false;
-        this.showTable = true;
         this.isAdvancedSearch = false;
+        this.landen = [];
+        this.gemeenten = [];
+        this.postcodes = [];
+        this.straten = [];
+        this.huisnrs = [];
+        this.suggest = {};
+        this.filters = {};
+        this.gridOptions = {};
+        this.gridOptions.context = this;
+        this.gridOptions.enableColResize = true;
+        this.gridOptions.suppressMovableColumns = true;
+        this.gridOptions.suppressClickEdit = true;
+        this.gridOptions.enableServerSideSorting = true;
+        this.gridOptions.headerHeight = 30;
+        this.gridOptions.rowHeight = 25;
+        this.gridOptions.rowModelType = 'infinite';
+        this.gridOptions.rowData = null;
+        this.gridOptions.infiniteInitialRowCount = 1;
+        this.gridOptions.cacheBlockSize = 50;
+        this.loadLanden();
+        this.suggest.gemeenten = { suggest: function (value) { return _this.loadGemeenten(value); } };
+        this.suggest.postcode = { suggest: function (value) { return _this.loadPostcodes(value); } };
+        this.suggest.straten = { suggest: function (value) { return _this.loadStraten(value); } };
+        this.suggest.huisnummer = { suggest: function (value) { return _this.loadHuisnrs(value); } };
     }
-    ActorWidget.prototype.toggleFilters = function (activate) {
-        this.showTable = !activate;
-        this.showActor = false;
-        this.showFilters = activate;
+    ActorWidget.prototype.setRowData = function () {
+        var dataSource = {
+            rowCount: null,
+            getRows: function (params) {
+                params.context.showSpinner = true;
+                var paramsObj = {};
+                if (params.context.isAdvancedSearch) {
+                    var f = params.context.filters;
+                    paramsObj = {
+                        naam: f.naam ? f.naam : undefined,
+                        voornaam: f.voornaam ? f.voornaam : undefined,
+                        email: f.email ? f.email : undefined,
+                        telefoon: f.telefoon ? f.telefoon : undefined,
+                        type: f.type ? f.type : undefined,
+                        land: f.land ? f.land : undefined,
+                        gemeente: f.gemeente && f.gemeente !== null ? f.gemeente.id : undefined,
+                        postcode: f.postcode && f.postcode !== null ? f.postcode.id : f.post_code || undefined,
+                        straat: f.straat && f.straat != null ? f.straat.id : undefined,
+                        straat_naam: f.straat_naam ? f.straat_naam : undefined,
+                        huisnummer: f.huisnummer && f.huisnummer !== null ? f.huisnummer.id : undefined,
+                        huisnummer_label: f.huisnummer_label ? f.huisnummer_label : undefined,
+                        subadres: f.subadres ? f.subadres : undefined,
+                        persid: f.persid ? f.persid : undefined,
+                        rrn: f.rrn ? f.rrn : undefined,
+                        kbo: f.kbo ? f.kbo : undefined
+                    };
+                }
+                else {
+                    var query = params.context.zoekterm || null;
+                    var sort = '+';
+                    if (params.sortModel.length) {
+                        var sortModel = params.sortModel[0];
+                        sort = ((sortModel.sort === 'asc') ? '' : '-') + sortModel.colId;
+                    }
+                    paramsObj = {
+                        omschrijving: query ? query + '*' : null,
+                        sort: sort
+                    };
+                }
+                params.context.apiService.getActoren(params.startRow, params.endRow, paramsObj)
+                    .then(function (data) {
+                    if (data) {
+                        params.successCallback(data.content, data.lastRow);
+                        params.context.zoekterm = '';
+                    }
+                    params.context.onGridSizeChanged();
+                    params.context.showSpinner = false;
+                });
+            }
+        };
+        this.gridOptions.api.setDatasource(dataSource);
+    };
+    ActorWidget.prototype.keydown = function (e) {
+        var key = e.which;
+        if (key === 13) {
+            e.preventDefault();
+            this.search();
+        }
+        return true;
+    };
+    ActorWidget.prototype.onGridReady = function () {
+        this.setRowData();
+    };
+    ActorWidget.prototype.onGridSizeChanged = function () {
+        console.debug('actor-widget::onGridSizeChanged');
+        if (this.gridOptions.api) {
+            this.gridOptions.api.sizeColumnsToFit();
+        }
+    };
+    ActorWidget.prototype.search = function () {
+        this.gridOptions.api.purgeInfiniteCache();
+    };
+    ActorWidget.prototype.refresh = function () {
+        this.gridOptions.api.clearFocusedCell();
+        this.selectedActor = undefined;
+        this.isAdvancedSearch = false;
+        this.setRowData();
     };
     ActorWidget.prototype.advancedSearch = function () {
         this.isAdvancedSearch = true;
         this.showFilters = false;
         this.showTable = true;
     };
+    ActorWidget.prototype.clearFilters = function () {
+        this.filters = {};
+        this.refresh();
+    };
+    ActorWidget.prototype.toggleFilters = function (activate) {
+        this.showTable = !activate;
+        this.showActor = false;
+        this.showFilters = activate;
+    };
+    ActorWidget.prototype.selectActor = function (params) {
+        this.selectedActor = params.data;
+    };
+    ActorWidget.prototype.toggleActorDetail = function (activate, params) {
+        var _this = this;
+        if (activate) {
+            this.showSpinner = true;
+            this.actorenApiService.getActorById(params.data.id)
+                .then(function (data) {
+                _this.showSpinner = false;
+                if (data) {
+                    _this.selectedActor = data;
+                    _this.showTable = !activate;
+                    _this.showActor = activate;
+                }
+            });
+        }
+        else {
+            this.showTable = !activate;
+            this.showActor = activate;
+        }
+    };
+    ActorWidget.prototype.loadLanden = function () {
+        var _this = this;
+        this.crabService.getLanden().then(function (landen) {
+            if (landen) {
+                var firstOptions = [
+                    { id: 'BE', naam: 'België' },
+                    { id: 'DE', naam: 'Duitsland' },
+                    { id: 'FR', naam: 'Frankrijk' },
+                    { id: 'GB', naam: 'Groot-Brittanië' },
+                    { id: 'NL', naam: 'Nederland' },
+                    { id: 'LU', naam: 'Luxemburg' },
+                    { naam: '─────────────────────────', disabled: true }
+                ];
+                _this.landen = firstOptions;
+                landen.forEach(function (land) {
+                    var exists = _this.landen.find(function (obj) { return obj.id === land.id; });
+                    if (!exists) {
+                        _this.landen.push(land);
+                    }
+                });
+            }
+        }).catch(function (error) {
+            console.debug(error);
+        });
+    };
+    ActorWidget.prototype.loadGemeenten = function (value) {
+        var _this = this;
+        return new Promise(function (resolve) {
+            _this.crabService.getGemeenten().then(function (gemeenten) {
+                if (gemeenten) {
+                    var result = gemeenten.filter(function (obj) { return obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1; });
+                    resolve(result);
+                }
+            });
+        });
+    };
+    ActorWidget.prototype.loadPostcodes = function (value) {
+        var _this = this;
+        var gemeente = this.filters.gemeente ? this.filters.gemeente.id : undefined;
+        return new Promise(function (resolve) {
+            if (gemeente) {
+                _this.crabService.getPostcodes(gemeente).then(function (postcodes) {
+                    postcodes.forEach(function (postcode) {
+                        postcode.naam = String(postcode.id);
+                    });
+                    var result = postcodes.filter(function (obj) { return obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1; });
+                    resolve(result);
+                });
+            }
+            else {
+                _this.filters.post_code = value;
+            }
+        });
+    };
+    ActorWidget.prototype.loadStraten = function (value) {
+        var _this = this;
+        var gemeente = this.filters.gemeente ? this.filters.gemeente.id : undefined;
+        return new Promise(function (resolve) {
+            if (gemeente) {
+                _this.crabService.getStraten(gemeente).then(function (straten) {
+                    var result = straten.filter(function (obj) { return obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1; });
+                    _this.filters.straat_naam = undefined;
+                    resolve(result);
+                });
+            }
+            else {
+                _this.filters.straat_naam = value;
+            }
+        });
+    };
+    ActorWidget.prototype.loadHuisnrs = function (value) {
+        var _this = this;
+        var straat = this.filters.straat ? this.filters.straat.id : undefined;
+        return new Promise(function (resolve) {
+            if (straat) {
+                _this.crabService.getHuisnrs(straat).then(function (huisnrs) {
+                    var result = huisnrs.filter(function (obj) { return obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1; });
+                    _this.filters.huisnummer_label = undefined;
+                    resolve(result);
+                });
+            }
+            else {
+                _this.filters.huisnummer_label = value;
+            }
+        });
+    };
+    __decorate([
+        aurelia_templating_1.bindable,
+        __metadata("design:type", Object)
+    ], ActorWidget.prototype, "actorenApiService", void 0);
+    ActorWidget = __decorate([
+        aurelia_framework_1.autoinject,
+        __metadata("design:paramtypes", [crab_api_service_1.CrabService])
+    ], ActorWidget);
     return ActorWidget;
 }());
 exports.ActorWidget = ActorWidget;
--- src/actor-widget/actor-widget.ts
+++ src/actor-widget/actor-widget.ts
@@ -1,14 +1,133 @@
+import { bindable } from 'aurelia-templating';
+import { GridOptions } from 'ag-grid';
+import { CrabService } from 'services/crab.api-service';
+import { autoinject } from 'aurelia-framework';
+
+@autoinject
 export class ActorWidget {
+  @bindable public actorenApiService: any;
+
+  public showSpinner: boolean = true;
+  public gridOptions: GridOptions;
+  public zoekterm: string;
+  public selectedActor: any;
+  public showTable: boolean = true;
   public showActor: boolean = false;
   public showFilters: boolean = false;
-  public showTable: boolean = true;
-  public selectedActor: any;
   public isAdvancedSearch: boolean = false;
-  
-  public toggleFilters(activate: boolean) {
-    this.showTable = !activate;
-    this.showActor = false;
-    this.showFilters = activate;
+  public landen: any[] = [];
+  public gemeenten: any[] = [];
+  public postcodes: any[] = [];
+  public straten: any[] = [];
+  public huisnrs: any[] = [];
+  public suggest: any = {};
+
+  private filters: any = {};
+
+  constructor(private crabService: CrabService) {
+    this.gridOptions = {} as GridOptions;
+    this.gridOptions.context = this;
+    this.gridOptions.enableColResize = true;
+    this.gridOptions.suppressMovableColumns = true;
+    this.gridOptions.suppressClickEdit = true;
+    this.gridOptions.enableServerSideSorting = true;
+    this.gridOptions.headerHeight = 30;
+    this.gridOptions.rowHeight = 25;
+    this.gridOptions.rowModelType = 'infinite';
+    this.gridOptions.rowData = null;
+    this.gridOptions.infiniteInitialRowCount = 1;
+    this.gridOptions.cacheBlockSize = 50;
+
+    this.loadLanden();
+    this.suggest.gemeenten = { suggest: value => this.loadGemeenten(value) };
+    this.suggest.postcode = { suggest: value => this.loadPostcodes(value) };
+    this.suggest.straten = { suggest: value => this.loadStraten(value) };
+    this.suggest.huisnummer = { suggest: value => this.loadHuisnrs(value) };
+  }
+
+  public setRowData() {
+    const dataSource = {
+      rowCount: null,
+      getRows: params => {
+        params.context.showSpinner = true;
+
+        let paramsObj = {};
+        if (params.context.isAdvancedSearch) {
+          const f = params.context.filters;
+          paramsObj = {
+            naam: f.naam ? f.naam : undefined,
+            voornaam: f.voornaam ? f.voornaam : undefined,
+            email: f.email ? f.email : undefined,
+            telefoon: f.telefoon ? f.telefoon : undefined,
+            type: f.type ? f.type : undefined,
+            land: f.land ? f.land : undefined,
+            gemeente: f.gemeente && f.gemeente !== null ? f.gemeente.id : undefined,
+            postcode: f.postcode && f.postcode !== null ? f.postcode.id : f.post_code || undefined,
+            straat: f.straat && f.straat != null ? f.straat.id : undefined,
+            straat_naam: f.straat_naam ? f.straat_naam : undefined,
+            huisnummer: f.huisnummer && f.huisnummer !== null ? f.huisnummer.id : undefined,
+            huisnummer_label: f.huisnummer_label ? f.huisnummer_label : undefined,
+            subadres: f.subadres ? f.subadres : undefined,
+            persid: f.persid ? f.persid : undefined,
+            rrn: f.rrn ? f.rrn : undefined,
+            kbo: f.kbo ? f.kbo : undefined
+          };
+        } else {
+          const query = params.context.zoekterm || null;
+          let sort: string = '+';
+          if (params.sortModel.length) {
+            const sortModel = params.sortModel[0];
+            sort = ((sortModel.sort === 'asc') ? '' : '-') + sortModel.colId;
+          }
+          paramsObj = {
+            omschrijving: query ? query + '*' : null,
+            sort: sort
+          };
+        }
+
+        params.context.apiService.getActoren(params.startRow, params.endRow, paramsObj)
+          .then(data => {
+            if (data) {
+              params.successCallback(data.content, data.lastRow);
+              params.context.zoekterm = '';
+            }
+            params.context.onGridSizeChanged();
+            params.context.showSpinner = false;
+          });
+      }
+    };
+    this.gridOptions.api.setDatasource(dataSource);
+  }
+
+  public keydown(e) {
+    const key = e.which;
+    if (key === 13) {
+      e.preventDefault();
+      this.search();
+    }
+    return true;
+  }
+
+  public onGridReady() {
+    this.setRowData();
+  }
+
+  public onGridSizeChanged() {
+    console.debug('actor-widget::onGridSizeChanged');
+    if (this.gridOptions.api) {
+      this.gridOptions.api.sizeColumnsToFit();
+    }
+  }
+
+  public search() {
+    this.gridOptions.api.purgeInfiniteCache();
+  }
+
+  public refresh() {
+    this.gridOptions.api.clearFocusedCell();
+    this.selectedActor = undefined;
+    this.isAdvancedSearch = false;
+    this.setRowData();
   }
 
   public advancedSearch() {
@@ -16,4 +135,120 @@ export class ActorWidget {
     this.showFilters = false;
     this.showTable = true;
   }
- }
+
+  public clearFilters() {
+    this.filters = {};
+    this.refresh();
+  }
+
+  public toggleFilters(activate: boolean) {
+    this.showTable = !activate;
+    this.showActor = false;
+    this.showFilters = activate;
+  }
+
+  public selectActor(params) {
+    this.selectedActor = params.data;
+  }
+
+  public toggleActorDetail(activate: boolean, params) {
+    if (activate) {
+      this.showSpinner = true;
+      this.actorenApiService.getActorById(params.data.id)
+        .then(data => {
+          this.showSpinner = false;
+          if (data) {
+            this.selectedActor = data;
+            this.showTable = !activate;
+            this.showActor = activate;
+          }
+        });
+    } else {
+      this.showTable = !activate;
+      this.showActor = activate;
+    }
+  }
+
+  private loadLanden() {
+    this.crabService.getLanden().then(landen => {
+      if (landen) {
+        const firstOptions = [
+          { id: 'BE', naam: 'België' },
+          { id: 'DE', naam: 'Duitsland' },
+          { id: 'FR', naam: 'Frankrijk' },
+          { id: 'GB', naam: 'Groot-Brittanië' },
+          { id: 'NL', naam: 'Nederland' },
+          { id: 'LU', naam: 'Luxemburg' },
+          { naam: '─────────────────────────', disabled: true }
+        ];
+        this.landen = firstOptions;
+        landen.forEach(land => {
+          const exists = this.landen.find(obj => obj.id === land.id);
+          if (!exists) {
+            this.landen.push(land);
+          }
+        });
+      }
+    }).catch(error => {
+      console.debug(error);
+    });
+  }
+
+  private loadGemeenten(value: string) {
+    return new Promise(resolve => {
+      this.crabService.getGemeenten().then(gemeenten => {
+        if (gemeenten) {
+          const result = gemeenten.filter(obj => obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  private loadPostcodes(value: string) {
+    const gemeente = this.filters.gemeente ? this.filters.gemeente.id : undefined;
+    return new Promise((resolve) => {
+      if (gemeente) {
+        this.crabService.getPostcodes(gemeente).then(postcodes => {
+          postcodes.forEach(postcode => {
+            postcode.naam = String(postcode.id);
+          });
+          const result = postcodes.filter(obj => obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+          resolve(result);
+        });
+      } else {
+        this.filters.post_code = value;
+      }
+    });
+  }
+
+  private loadStraten(value: string) {
+    const gemeente = this.filters.gemeente ? this.filters.gemeente.id : undefined;
+    return new Promise((resolve) => {
+      if (gemeente) {
+        this.crabService.getStraten(gemeente).then(straten => {
+          const result = straten.filter(obj => obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+          this.filters.straat_naam = undefined;
+          resolve(result);
+        });
+      } else {
+        this.filters.straat_naam = value;
+      }
+    });
+  }
+
+  private loadHuisnrs(value: string) {
+    const straat = this.filters.straat ? this.filters.straat.id : undefined;
+    return new Promise((resolve) => {
+      if (straat) {
+        this.crabService.getHuisnrs(straat).then(huisnrs => {
+          const result = huisnrs.filter(obj => obj.naam.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+          this.filters.huisnummer_label = undefined;
+          resolve(result);
+        });
+      } else {
+        this.filters.huisnummer_label = value;
+      }
+    });
+  }
+}
