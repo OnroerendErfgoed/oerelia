5ca552703dc83386d5b1e53a8ba81f8eaae7e8cb
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2021-11-04 16:53:07 +0100

    test new geolocate pt4

--- src/gis-utils/components/ol-geolocate.ts
+++ src/gis-utils/components/ol-geolocate.ts
@@ -5,6 +5,7 @@ export class Geolocate extends ol.control.Control {
   public element: Element;
   public button: HTMLButtonElement;
   public geolocation: ol.Geolocation;
+  public positionFeature: ol.Feature;
 
   constructor(optOptions: any) {
     console.debug('Geolocate::constructor', optOptions);
@@ -21,30 +22,49 @@ export class Geolocate extends ol.control.Control {
     this.button.innerHTML = '<i class="fa fa-map-marker"></i>';
     this.element.appendChild(this.button);
 
-    // this.geolocation = new ol.Geolocation({
-    //   projection: this.options.projection,
-    //   trackingOptions: {
-    //     enableHighAccuracy: true
-    //   }
-    // });
+    this.geolocation = new ol.Geolocation({
+      projection: this.getMap().getView().getProjection(),
+      trackingOptions: {
+        enableHighAccuracy: true
+      }
+    });
 
-    this.button.onclick = () => {
-      console.debug('onclick');
-      // this._zoomToLocation();
-    };
+    this.positionFeature = new ol.Feature();
+    this.positionFeature.setStyle(
+      new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 6,
+          fill: new ol.style.Fill({
+            color: '#3399CC',
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#fff',
+            width: 2,
+          }),
+        }),
+      })
+    );
+
+
+    this.button.addEventListener('click', this._zoomToLocation.bind(this), false);
 
     ol.control.Control.call(this, {
       element: this.element,
       target: this.options.target
     });
-  }
 
-  click() {
-    console.debug('click');
-    console.debug(this.getMap());
+    const map = this.getMap();
+    const source = new ol.source.Vector({
+      features: [this.positionFeature]
+    });
+    const layer = new ol.layer.Vector({
+      source: source
+    });
+    map.addLayer(layer);
   }
 
   private _zoomToLocation() {
+    console.debug('_zoomToLocation');
     if (!this.geolocation) {
       return;
     }
@@ -52,31 +72,32 @@ export class Geolocate extends ol.control.Control {
     const map = this.getMap();
     const view = map.getView();
     this.geolocation.setTracking(true);
-    this.geolocation.once('change:position', () => {
-      const position = this.geolocation.getPosition();
-      view.setCenter(position);
+    this.geolocation.on('change:position', () => {
+      const coordinates = this.geolocation.getPosition();
+      view.setCenter(coordinates);
       if (zoomLevel) {
         view.setZoom(zoomLevel);
       }
       this.geolocation.setTracking(false);
+      this.positionFeature.setGeometry(coordinates ? new ol.geom.Point(coordinates) : null);
 
-      const marker = document.getElementById('marker');
-      marker.classList.remove('hide');
-      const overlayId = 'markerOverlay';
-      const overlay = map.getOverlayById(overlayId);
-      if (!overlay) {
-        map.addOverlay(
-          new ol.Overlay({
-            id: overlayId,
-            position: position,
-            positioning: 'center-center',
-            element: marker,
-            stopEvent: false
-          })
-        );
-      } else {
-        overlay.setPosition(position);
-      }
+      // const marker = document.getElementById('marker');
+      // marker.classList.remove('hide');
+      // const overlayId = 'markerOverlay';
+      // const overlay = map.getOverlayById(overlayId);
+      // if (!overlay) {
+      //   map.addOverlay(
+      //     new ol.Overlay({
+      //       id: overlayId,
+      //       position: position,
+      //       positioning: 'center-center',
+      //       element: marker,
+      //       stopEvent: false
+      //     })
+      //   );
+      // } else {
+      //   overlay.setPosition(position);
+      // }
     });
   }
 }
