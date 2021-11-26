bf87454e09dbd0d4d1d18a8d1a53a01f6b602ce9
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2021-11-26 10:43:33 +0100

    map configureerbaar

--- dist/commonjs/gis-utils/map-util.d.ts
+++ dist/commonjs/gis-utils/map-util.d.ts
@@ -1,5 +1,6 @@
 import * as ol from 'openlayers';
 import { Boundingbox } from './models/boundingbox';
+import { MapConfig } from './models/map-config';
 export declare class MapUtil {
     static transformBoundingboxToMapExtent(boundingbox: Boundingbox): [number, number, number, number];
     static transformLatLonToPoint(lat: number, lon: number): ol.geom.Point;
@@ -7,7 +8,7 @@ export declare class MapUtil {
     static createVectorLayer(options: any): ol.layer.Vector;
     static createNgiLayer(layerId: string, title: string, isBaseLayer: boolean): ol.layer.Layer;
     static createGrbWMSLayer(wmsLayers: string, title: string, isBaseLayer: boolean, mapProjection: ol.proj.Projection): ol.layer.Tile;
-    static createMap(target: Element, mapProjection: ol.proj.Projection): ol.Map;
+    static createMap(target: Element, config: MapConfig): ol.Map;
     static mergePolygons(features: ol.Feature[]): ol.Feature;
     static intersectPolygons(polygon1: ol.Feature, polygon2: ol.Feature): ol.Feature;
     static subtractPolygons(polygon1: ol.Feature, polygon2: ol.Feature): ol.Feature;
--- dist/native-modules/gis-utils/map-util.js
+++ dist/native-modules/gis-utils/map-util.js
@@ -155,15 +155,16 @@ var MapUtil = (function () {
         layer.set('type', isBaseLayer ? 'base' : 'overlay');
         return layer;
     };
-    MapUtil.createMap = function (target, mapProjection) {
+    MapUtil.createMap = function (target, config) {
         var map = new ol.Map({
             layers: [],
             target: target,
             view: new ol.View({
-                center: ol.extent.getCenter(mapProjection.getExtent()),
-                projection: mapProjection,
-                zoom: 2,
-                maxZoom: 21
+                center: config.center || ol.extent.getCenter(config.mapProjection.getExtent()),
+                projection: config.mapProjection,
+                zoom: config.zoom || 2,
+                maxZoom: config.maxZoom || 21,
+                minZoom: config.minZoom || 1
             }),
             controls: ol.control.defaults({
                 attributionOptions: ({
@@ -175,16 +176,20 @@ var MapUtil = (function () {
             logo: false
         });
         map.addControl(new ol.control.ScaleLine());
-        map.addControl(new Geolocate({}));
-        var layerswitcherPanel = new LayerswitcherPanel({
-            title: 'Legende'
-        });
-        map.addControl(new Layerswitcher({
-            tipLabel: 'Verander de kaartlagen',
-            title: 'Kaartlagen',
-            panel: layerswitcherPanel
-        }));
-        map.addControl(layerswitcherPanel);
+        if (config.useGeolocate) {
+            map.addControl(new Geolocate({ zoomLevel: config.geolocateZoom }));
+        }
+        if (config.useLayerswitcher) {
+            var layerswitcherPanel = new LayerswitcherPanel({
+                title: 'Legende'
+            });
+            map.addControl(new Layerswitcher({
+                tipLabel: 'Verander de kaartlagen',
+                title: 'Kaartlagen',
+                panel: layerswitcherPanel
+            }));
+            map.addControl(layerswitcherPanel);
+        }
         return map;
     };
     MapUtil.mergePolygons = function (features) {
