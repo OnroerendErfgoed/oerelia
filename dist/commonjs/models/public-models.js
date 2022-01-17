c18d9601f9c301aaf6054b4722449a736cb7dd61
Yannick Kuypers <Kuypers.y@gmail.com>
2022-01-17 16:32:38 +0100

    #145 add interface models

---
+++ dist/commonjs/models/public-models.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+
+//# sourceMappingURL=public-models.js.map
---
+++ src/models/models.ts
@@ -0,0 +1,70 @@
+interface IType {
+  id: number;
+  naam: string;
+  uri?: string;
+}
+
+interface IAdres {
+  adrestype: IType;
+  beschrijving: string;
+  einddatum: string;
+  gemeente: string;
+  gemeente_id: number;
+  huisnummer: string;
+  huisnummer_id: number;
+  id: number;
+  land: string;
+  land_omschrijving: string;
+  omschrijving: string;
+  postcode: string;
+  startdatum: string;
+  straat: string;
+  straat_id: number;
+  subadres: string
+  subadres_id: number;
+}
+
+interface IEmail {
+  email: string;
+  type: IType;
+}
+
+interface IRedenErkenning {
+  id: number;
+  reden_erkenning: string;
+}
+
+interface IId {
+  extra_id: string;
+  type: IType;
+}
+
+interface IRelatie {
+  einddatum: string;
+  id: number;
+  omschrijving: string;
+  startdatum: string;
+  type: IType;
+}
+
+interface IActorStatusGebruiker {
+  uri: string;
+  omschrijving: string;
+}
+
+interface IActorStatusStatus {
+  id: number;
+  status: string;
+}
+
+interface IActorStatus {
+  datum: string;
+  gebruiker: IActorStatusGebruiker;
+  opmerkingen: string;
+  status: IActorStatusStatus;
+}
+
+interface ISystemActor {
+  uri: string;
+  description?: string;
+}
