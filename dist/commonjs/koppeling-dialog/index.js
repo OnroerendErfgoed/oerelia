f55a1a901fecc906c1016a2effd7e490508021c7
Yannick Kuypers <kuypers.y@gmail.com>
2022-08-19 14:12:54 +0200

    #218 fix koppeling-dialog

--- dist/commonjs/koppeling-dialog/index.js
+++ dist/commonjs/koppeling-dialog/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var aurelia_framework_1 = require("aurelia-framework");
 function configure(config) {
     config.globalResources([
-        aurelia_framework_1.PLATFORM.moduleName('../expander')
+        aurelia_framework_1.PLATFORM.moduleName('./koppeling-dialog')
     ]);
 }
 exports.configure = configure;
--- src/koppeling-dialog/index.ts
+++ src/koppeling-dialog/index.ts
@@ -2,6 +2,6 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('../expander')
+    PLATFORM.moduleName('./koppeling-dialog')
   ]);
 }
