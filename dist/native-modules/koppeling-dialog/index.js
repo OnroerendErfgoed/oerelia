c4f860c196bbd86979ea36e87710bb5646cfee35
Yannick Kuypers <kuypers.y@gmail.com>
2022-08-19 14:04:13 +0200

    #218 expander openstellen voor koppeling-dialog

--- dist/native-modules/koppeling-dialog/index.js
+++ dist/native-modules/koppeling-dialog/index.js
@@ -1,7 +1,7 @@
 import { PLATFORM } from 'aurelia-framework';
 export function configure(config) {
     config.globalResources([
-        PLATFORM.moduleName('./expander')
+        PLATFORM.moduleName('../expander')
     ]);
 }
 
