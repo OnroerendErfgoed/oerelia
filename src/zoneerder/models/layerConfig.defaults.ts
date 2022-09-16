9fdd821bcabfe3a325b787746d8c1d611ea35050
Jonas Geuens <jonasgeuens@gmail.com>
2022-09-16 11:14:46 +0200

    move vast_la to separate layer

--- src/zoneerder/models/layerConfig.defaults.ts
+++ src/zoneerder/models/layerConfig.defaults.ts
@@ -21,13 +21,19 @@ export const defaultLayerConfig: LayerConfig = {
         'vioe_geoportaal:bes_arch_site,' +
         'vioe_geoportaal:bes_monument,' +
         'vioe_geoportaal:bes_overgangszone,' +
-        'vioe_geoportaal:erfgoedls,' +
-        'vioe_geoportaal:vast_la', title: 'Beschermd Onroerend Erfgoed'
+        'vioe_geoportaal:erfgoedls',
+        title: 'Beschermd Onroerend Erfgoed'
     },
     gga: {
       type: LayerType.ErfgoedWms,
       wmsLayers: 'vioe_geoportaal:gga_gewestelijk,vioe_geoportaal:gga_gemeentelijk',
       title: 'Gebieden geen archeologie'
+    },
+    vast: {
+      type: LayerType.ErfgoedWms,
+      wmsLayers: 'vioe_geoportaal:vast_la',
+      title: 'Vastgesteld landschapsrelict',
+      visible: false
     },
     UNESCO: {
       type: LayerType.ErfgoedWms,
