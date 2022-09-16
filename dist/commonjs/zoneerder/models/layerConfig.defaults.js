6500833dfa0b72c36184ca1116cd8249c36b096d
Jonas Geuens <jonasgeuens@gmail.com>
2022-09-16 11:59:58 +0200

    223 UNESCO toevoegen aan Zoneerder layerconfig (#224)
    * #223 UNESCO en vast_la toegevoegd
    
    * cleanup
    
    * move vast_la to separate layer

--- dist/commonjs/zoneerder/models/layerConfig.defaults.js
+++ dist/commonjs/zoneerder/models/layerConfig.defaults.js
@@ -21,12 +21,26 @@ exports.defaultLayerConfig = {
                 'vioe_geoportaal:bes_arch_site,' +
                 'vioe_geoportaal:bes_monument,' +
                 'vioe_geoportaal:bes_overgangszone,' +
-                'vioe_geoportaal:erfgoedls', title: 'Beschermd Onroerend Erfgoed'
+                'vioe_geoportaal:erfgoedls',
+            title: 'Beschermd Onroerend Erfgoed'
         },
         gga: {
             type: layerConfig_enums_1.LayerType.ErfgoedWms,
             wmsLayers: 'vioe_geoportaal:gga_gewestelijk,vioe_geoportaal:gga_gemeentelijk',
             title: 'Gebieden geen archeologie'
+        },
+        vast: {
+            type: layerConfig_enums_1.LayerType.ErfgoedWms,
+            wmsLayers: 'vioe_geoportaal:vast_la',
+            title: 'Vastgesteld landschapsrelict',
+            visible: false
+        },
+        UNESCO: {
+            type: layerConfig_enums_1.LayerType.ErfgoedWms,
+            wmsLayers: 'vioe_geoportaal:unesco_kern,' +
+                'vioe_geoportaal:unesco_buffer',
+            title: 'UNESCO',
+            visible: false
         }
     }
 };
