ad7aa9df6a39d2d470ef845075042752430142f3
Emrys Roef <emrys.roef@geosolutions.be>
2024-11-18 11:53:47 +0100

    #468 predictions niet nul als default

--- dist/native-modules/zoneerder/components/referentielaag-autocorrectie.js
+++ dist/native-modules/zoneerder/components/referentielaag-autocorrectie.js
@@ -128,10 +128,11 @@ var ReferentielaagAutocorrectie = (function () {
                         return [4, this.alignGrb(this.zone, this.referentielaag.value, this.domeinstrategie.value)];
                     case 2:
                         _a.histogramData = _d.sent();
-                        this.relevanteAfstand = this.getRelevanteAfstand(this.histogramData.predictions);
+                        this.relevanteAfstanden = this.getRelevanteAfstanden(this.histogramData.predictions);
+                        this.relevanteAfstand = this.relevanteAfstanden[0];
                         this.laatstGealigneerd = new Date().toISOString();
                         this.loadingData = false;
-                        setupD3(this.histogram, this.histogramData.diffs, Number(this.relevanteAfstand));
+                        setupD3(this.histogram, this.histogramData.diffs, this.relevanteAfstanden.map(function (x) { return Number(x); }));
                         floatNumber = Number(this.relevanteAfstand).toFixed(1);
                         this.resultsUpdated(this.histogramData.series[floatNumber]);
                         data = Object.entries(this.histogramData.diffs).map(function (_a) {
@@ -162,10 +163,12 @@ var ReferentielaagAutocorrectie = (function () {
         }
         this.resultsUpdated(this.histogramData.series[floatNumber]);
     };
-    ReferentielaagAutocorrectie.prototype.getRelevanteAfstand = function (predictions) {
-        if (!predictions || Object.entries(predictions).length == 0)
-            return "0.0";
-        return Object.keys(predictions)[0];
+    ReferentielaagAutocorrectie.prototype.getRelevanteAfstanden = function (predictions) {
+        var distances = (!predictions || Object.entries(predictions).length == 0) ? ["0.0"] : Object.keys(predictions);
+        if (distances[0] === "0.0") {
+            distances.push(distances.shift());
+        }
+        return distances;
     };
     __decorate([
         bindable,
